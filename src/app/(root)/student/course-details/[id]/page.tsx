639843b483b88d171bd20e9877db9b7373bff6d3
Muh-Uzair <muhammaduzair1062001@gmail.com>
2026-05-21 01:03:04 +0500

    some updates

--- src/app/(root)/student/course-details/[id]/page.tsx
+++ src/app/(root)/student/course-details/[id]/page.tsx
@@ -1,7 +1,8 @@
 import CourseDetails from "@/features/course-management/ui/CourseDetails";
 
+
 const CourseDetailsPage = () => {
-  return <CourseDetails viewerRole="student" />;
+    return <CourseDetails viewerRole="student" />;
 };
 
 export default CourseDetailsPage;
--- src/features/student-browse-courses/ui/BrowseCourses.tsx
+++ src/features/student-browse-courses/ui/BrowseCourses.tsx
@@ -113,7 +113,7 @@ const BrowseCourses = () => {
             className="w-full"
             onClick={() =>
               router.push(
-                `/student/browse-courses/course-details/${course._id}`,
+                `/student/course-details/${course._id}`,
               )
             }
           >
